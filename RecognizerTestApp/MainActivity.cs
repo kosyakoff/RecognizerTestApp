b23cc387387de1b18859fb9bdd15e45ee331285d
sondor <sondor@arview.pro>
2019-08-22 21:36:02 +0700

    Google variant

--- RecognizerTestApp/MainActivity.cs
+++ RecognizerTestApp/MainActivity.cs
@@ -8,6 +8,7 @@ using System.Timers;
 using Android;
 using Android.App;
 using Android.Content.PM;
+using Android.Gms.Tasks;
 using Android.Gms.Vision;
 using Android.Gms.Vision.Texts;
 using Android.Graphics;
@@ -19,21 +20,52 @@ using Android.Support.V4.Content;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Firebase;
+using Firebase.ML.Vision;
+using Firebase.ML.Vision.Common;
+using Firebase.ML.Vision.Text;
 using Java.Nio;
+using Java.Util.Concurrent;
 using Tesseract;
 using Tesseract.Droid;
 using Camera = Android.Hardware.Camera;
+using Object = Java.Lang.Object;
 using Timer = System.Timers.Timer;
 
 namespace RecognizerTestApp
 {
+    public class ProcessImageListener : Java.Lang.Object, IOnSuccessListener, IOnCompleteListener, IOnFailureListener
+    {
+        public void OnFailure(Java.Lang.Exception e)
+        {
+            var tt = e;
+        }
+
+        public void OnComplete(Task task)
+        {
+            var tt = task;
+        }
+
+        public void OnSuccess(Object result)
+        {
+            var text = (FirebaseVisionText)result;
+        }
+    }
+
     [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
     public class MainActivity : AppCompatActivity, TextureView.ISurfaceTextureListener
     {
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
         private TextureView _textureView;
         private TextView _textView;
         private const int REQUEST_CAMERA_ID = 1001;
         private const int REQUEST_WRITE_ID = 1002;
+        private const int REQUEST_INTERNET_ID = 1003;
+        private ProcessImageListener _processImageListener;
         Camera _camera;
         private Camera.Size _previewSize;
         int _minX = int.MaxValue;
@@ -52,6 +84,8 @@ namespace RecognizerTestApp
 
         private string _referenceString = "рного тел ется, что и помощи  й можно п т в четыр ли овладе".Replace(" ","").ToLower();
 
+        private FirebaseApp _defaultFirebaseApp;
+
         private Timer _updateTimer = new Timer(700)
         {
             AutoReset = false
@@ -60,6 +94,7 @@ namespace RecognizerTestApp
         readonly int _accuracy = 10;
 
         Color _referenceColor = new Color(65, 113, 127);
+       // private Color _referenceColor = Color.Black;
         private List<Color> _referenceColors = new List<Color>
         {
             new Color(65, 113, 127),
@@ -73,9 +108,10 @@ namespace RecognizerTestApp
         {
 
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
 
-            
+            // Set our view from the "main" layout resource
+            _defaultFirebaseApp = FirebaseApp.InitializeApp(ApplicationContext);
+
             SetContentView(Resource.Layout.activity_main);
             _textView = FindViewById<TextView>(Resource.Id.text_view);
 
@@ -100,9 +136,17 @@ namespace RecognizerTestApp
                 ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.WriteExternalStorage },
                     REQUEST_WRITE_ID);
             }
+
+            if (ContextCompat.CheckSelfPermission(ApplicationContext, Manifest.Permission.Internet) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.Internet },
+                    REQUEST_INTERNET_ID);
+            }
+
+            _processImageListener = new ProcessImageListener();
         }
 
-        public async void OnSurfaceTextureAvailable(Android.Graphics.SurfaceTexture surface, int width, int height)
+        public void OnSurfaceTextureAvailable(Android.Graphics.SurfaceTexture surface, int width, int height)
         {
             if (Camera.NumberOfCameras == 0)
             {
@@ -135,11 +179,10 @@ namespace RecognizerTestApp
 
             // this is the sort of thing TextureView enables
             _textureView.Rotation = 90.0f;
-            //_textureView.Alpha = 0.5f;
 
-            _tesseractApi = new TesseractApi(ApplicationContext, AssetsDeployment.OncePerVersion);
-
-            await _tesseractApi.Init("rus");
+            //_tesseractApi = new TesseractApi(ApplicationContext, AssetsDeployment.OncePerVersion);
+            
+           // await _tesseractApi.Init("rus");
         }
 
         public bool OnSurfaceTextureDestroyed(Android.Graphics.SurfaceTexture surface)
@@ -164,9 +207,10 @@ namespace RecognizerTestApp
 
 
 
-        public async void OnSurfaceTextureUpdated(Android.Graphics.SurfaceTexture surface)
+        public void OnSurfaceTextureUpdated(Android.Graphics.SurfaceTexture surface)
         {
 
+            _textView.Text = $"Качество: 0%";
             if (_shouldUpdate)
             {
                 _updateTimer.Start();
@@ -183,29 +227,94 @@ namespace RecognizerTestApp
             _minY = int.MaxValue;
             _maxY = int.MinValue;
 
-            //var bitmap = _textureView.GetBitmap(_previewSize.Height, _previewSize.Width);
-            // var bitmap = _textureView.Bitmap;
+             //var bitmap = _textureView.Bitmap;
 
             _updateBitmap = _textureView.GetBitmap(_textureView.Bitmap.Width, _textureView.Bitmap.Height);
 
-//            if (!_screenshotTaken)
-//            {
-//                ExportBitmapAsPNG(_updateBitmap);
-//                _screenshotTaken = true;
-//            }
-          
-            using (Canvas canvas = new Canvas(_updateBitmap))
-            {
-                _textureView.Draw(canvas);
-            }
+            //using (Canvas canvas = new Canvas(_updateBitmap))
+            //{
+            //    _textureView.Draw(canvas);
+            //}
 
-            _updateBitmap.GetPixels(_pixelArray,0, _previewSize.Width, 0,0,_previewSize.Width, _previewSize.Height);
+            _updateBitmap.GetPixels(_pixelArray, 0, _previewSize.Width, 0, 0, _previewSize.Width, _previewSize.Height);
 
             var updateBitmapHeight = _previewSize.Height;
             var updateBitmapWidth = _previewSize.Width;
 
-           // List<Color> applicableColors = new List<Color>();
+            // List<Color> applicableColors = new List<Color>();
 
+            GetBoundingBox(updateBitmapHeight, updateBitmapWidth);
+
+            bool overlayWasHidden = _overlayView.Rect == null;
+            _overlayView.Rect = null;
+            if (_maxX > _minX && _maxY > _minY)
+            {
+                int searchBuffer = 10;
+
+                _overlayView.Rect = new Rect(_previewSize.Height - _maxY, _minX, _previewSize.Height - _minY, _maxX);
+
+                var matrix = new Matrix();
+                matrix.PostRotate(90);
+
+                var width = (_maxX - _minX);
+                var height = (_maxY - _minY);
+                var cropped = Bitmap.CreateBitmap(
+                    _updateBitmap,
+                    _minX > searchBuffer ? _minX - searchBuffer : _minX,
+                    _minY > searchBuffer ? _minY - searchBuffer : _minY,
+                    _minX + width <= _previewSize.Width - searchBuffer ? (_maxX - _minX) + searchBuffer : (_maxX - _minX),
+                    _minY + height <= _previewSize.Height - searchBuffer ? (_maxY - _minY) + searchBuffer : (_maxY - _minY),
+                    matrix,
+                    false);
+
+                //if (_tesseractApi != null && _tesseractApi.Initialized)
+                //{
+                //    var success = await _tesseractApi.Recognise(cropped);
+                //    if (success && _tesseractApi.Text.Length < 160)
+                //    {
+                //        string text = _tesseractApi.Text.Replace("\n", "").Replace(" ", "").ToLower();
+
+                //        var diff = StringDistance.GetDamerauLevenshteinDistance(_referenceString, text);
+
+                //        double quality = (((double)(_referenceString.Length - diff)) / _referenceString.Length) * 100;
+
+                //        _textView.Text = $"Качество: {quality}%";
+                //    }
+                //}
+
+                var image = FirebaseVisionImage.FromBitmap(cropped);
+                var firebaseVisionTextDetector = FirebaseVision.GetInstance(_defaultFirebaseApp).OnDeviceTextRecognizer;
+
+                try
+                {
+                    
+                    firebaseVisionTextDetector.ProcessImage(image).AddOnSuccessListener(_processImageListener);
+                    firebaseVisionTextDetector.ProcessImage(image).AddOnCompleteListener(_processImageListener);
+                    firebaseVisionTextDetector.ProcessImage(image).AddOnFailureListener(_processImageListener);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+              
+
+                //if (_redrawCount > 5)
+                //{
+                //    ExportBitmapAsPNG(cropped);
+                //    _redrawCount = 0;
+                //}
+            }
+
+            if (overlayWasHidden && _overlayView.Rect == null)
+            {
+                return;
+            }
+            _overlayView.ForceLayout();
+        }
+
+        private void GetBoundingBox(int updateBitmapHeight, int updateBitmapWidth)
+        {
             for (int j = 0; j < updateBitmapHeight; j++)
             {
                 for (int i = 0; i < updateBitmapWidth; i++)
@@ -214,10 +323,10 @@ namespace RecognizerTestApp
 
                     int red = Color.GetRedComponent(pixelColor);
 
-                   // applicableColors.AddRange(_referenceColors.Where(x => Math.Abs(x.R - red) < _accuracy));
+                    // applicableColors.AddRange(_referenceColors.Where(x => Math.Abs(x.R - red) < _accuracy));
 
                     if (Math.Abs(_referenceColor.R - red) >= _accuracy)
-                  //  if (!applicableColors.Any())
+                    //  if (!applicableColors.Any())
                     {
                         continue;
                     }
@@ -253,54 +362,6 @@ namespace RecognizerTestApp
                     //var distance = Math.Sqrt(dbl_test_blue + dbl_test_green + dbl_test_red);
                 }
             }
-
-            bool overlayWasHidden = _overlayView.Rect == null;
-            _overlayView.Rect = null;
-            if (_maxX > _minX && _maxY > _minY)
-            {
-                int searchBuffer = 10;
-
-                _overlayView.Rect = new Rect(_previewSize.Height - _maxY, _minX,  _previewSize.Height - _minY, _maxX);
-
-                var matrix = new Matrix();
-                matrix.PostRotate(90);
-                var cropped = Bitmap.CreateBitmap(
-                    _updateBitmap,
-                    _minX > searchBuffer ? _minX - searchBuffer : _minX,
-                    _minY > searchBuffer ? _minY - searchBuffer : _minY, 
-                    (_maxX - _minX) + searchBuffer, 
-                    (_maxY - _minY) + searchBuffer, 
-                    matrix, 
-                    false);
-
-                if (_tesseractApi != null && _tesseractApi.Initialized)
-                {
-                    var success = await _tesseractApi.Recognise(cropped);
-                    if (success && _tesseractApi.Text.Length < 160)
-                    {
-                        string text = _tesseractApi.Text.Replace("\n", "").Replace(" ", "").ToLower();
-
-                        var diff = StringDistance.GetDamerauLevenshteinDistance(_referenceString, text);
-
-                        double quality = (((double)(_referenceString.Length - diff)) / _referenceString.Length) * 100;
-
-                        _textView.Text = $"Качество: {quality}%";
-                    }
-                }
-
-
-                //if (_redrawCount > 5)
-                //{
-                //    ExportBitmapAsPNG(cropped);
-                //    _redrawCount = 0;
-                //}
-            }
-
-            if (overlayWasHidden && _overlayView.Rect == null)
-            {
-                return;
-            }
-            _overlayView.ForceLayout();
         }
 
         private void UpdateBoundingBox(int x, int y)
