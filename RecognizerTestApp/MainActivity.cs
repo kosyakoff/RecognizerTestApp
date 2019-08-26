33a7d34a41784c02c4a702c0415c110214219fd7
sondor <sondor@arview.pro>
2019-08-26 16:48:23 +0700

    Refactoring

--- RecognizerTestApp/MainActivity.cs
+++ RecognizerTestApp/MainActivity.cs
@@ -1,6 +1,10 @@
-﻿using Android;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Android;
 using Android.App;
 using Android.Content.PM;
+using Android.Gms.Tasks;
 using Android.Graphics;
 using Android.OS;
 using Android.Support.V4.App;
@@ -9,32 +13,27 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using Firebase;
-using Firebase.ML.Vision.Text;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Threading.Tasks;
 using Firebase.ML.Vision;
 using Firebase.ML.Vision.Common;
+using Firebase.ML.Vision.Text;
+using Recognizer.Core.Resources.Strings;
 using Tesseract.Droid;
 using Camera = Android.Hardware.Camera;
 using Environment = Android.OS.Environment;
 using Exception = Java.Lang.Exception;
 using Object = Java.Lang.Object;
 using Path = System.IO.Path;
-using Task = System.Threading.Tasks.Task;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace RecognizerTestApp
 {
-    public class ProcessImageListener : Object, Android.Gms.Tasks.IOnSuccessListener, 
-        Android.Gms.Tasks.IOnCompleteListener, Android.Gms.Tasks.IOnFailureListener
+    public class ProcessImageListener : Object, IOnSuccessListener,
+        IOnCompleteListener, IOnFailureListener
     {
         public EventHandler<string> TextRecognized;
 
-        public void OnComplete(Android.Gms.Tasks.Task task)
+        public void OnComplete(Task task)
         {
-
         }
 
         public void OnFailure(Exception e)
@@ -51,14 +50,14 @@ namespace RecognizerTestApp
 
     public class RecognitionResult
     {
-        public string ResultText { get; set; }
-        public double Quality { get; set; }
-        public Rect BoundingBox { get; set; }
-
         public RecognitionResult()
         {
             Invalidate();
         }
+
+        public string ResultText { get; set; }
+        public double Quality { get; set; }
+        public Rect BoundingBox { get; set; }
 
         public void Invalidate()
         {
@@ -68,54 +67,42 @@ namespace RecognizerTestApp
         }
     }
 
-    [Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, TextureView.ISurfaceTextureListener
     {
         private const int REQUEST_CAMERA_ID = 1001;
         private const int REQUEST_WRITE_ID = 1002;
         private const int REQUEST_INTERNET_ID = 1003;
 
-        public volatile bool _recognizingTextInProgress;
-
         private const int PRIMARY_ACCURACY = 15;
         private const int BIGGER_ACCURACY = 20;
         private const int SMALLER_ACCURACY = 10;
 
-        private int _currentAccuracy = PRIMARY_ACCURACY;
-
         private readonly Rect _bBox = new Rect();
-
-        readonly RecognitionResult _tempRecognitionResult = new RecognitionResult();
-        readonly RecognitionResult _finalRecognitionResult = new RecognitionResult();
-
-        private Camera _camera;
-
-        private FirebaseApp _defaultFirebaseApp;
-        private FirebaseVisionTextRecognizer _firebaseVisionTextDetector;
-        private ProcessImageListener _firebaseProcessImageListener;
-
-        private int[] _bitmapPixelArray;
-        private Camera.Size _previewSize;
-
-        private TesseractApi _tesseractApi;
+        private readonly RecognitionResult _finalRecognitionResult = new RecognitionResult();
 
         private readonly Color _referenceColor = new Color(65, 113, 127);
-
-        private List<Color> _referenceColors = new List<Color>
-        {
-            new Color(65, 113, 127),
-            new Color(83, 116, 125),
-            new Color(59, 96, 105)
-        };
 
         private readonly string _referenceString =
             "рного тел ется, что и помощи  й можно п т в четыр ли овладе".Replace(" ", "").ToLower();
 
-       // private bool _shouldUpdate = true;
+        private readonly RecognitionResult _tempRecognitionResult = new RecognitionResult();
 
-        private TextureView _textureView;
-        private TextView _textView;
+        private int[] _bitmapPixelArray;
+
+        private Camera _camera;
+
+        private int _currentAccuracy = PRIMARY_ACCURACY;
+
+        private FirebaseApp _defaultFirebaseApp;
+        private ProcessImageListener _firebaseProcessImageListener;
+        private FirebaseVisionTextRecognizer _firebaseVisionTextDetector;
         private OverlayView _overlayView;
+        private Camera.Size _previewSize;
+
+        private RecognizingActor _recognizingActor;
+
+        public volatile bool _recognizingTextInProgress;
 
         //private readonly Timer _updateTimer = new Timer(500)
         //{
@@ -124,11 +111,26 @@ namespace RecognizerTestApp
 
         private int _redrawCount;
 
-        public async void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        private List<Color> _referenceColors = new List<Color>
+        {
+            new Color(65, 113, 127),
+            new Color(83, 116, 125),
+            new Color(59, 96, 105)
+        };
+
+        private TesseractApi _tesseractApi;
+
+        // private bool _shouldUpdate = true;
+
+        private TextureView _textureView;
+        private TextView _textView;
+        private Toolbar _toolbar;
+
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
         {
             if (Camera.NumberOfCameras == 0)
             {
-                Toast.MakeText(this, "No camera", ToastLength.Long).Show();
+                Toast.MakeText(this, CommonResources.no_camera, ToastLength.Long).Show();
                 return;
             }
 
@@ -145,16 +147,13 @@ namespace RecognizerTestApp
             _textureView.LayoutParameters =
                 new FrameLayout.LayoutParams(_previewSize.Width, _previewSize.Height, GravityFlags.Center);
 
-            _bitmapPixelArray = new int[_previewSize.Width * _previewSize.Height];
-
             _camera.SetPreviewTexture(surface);
             _camera.StartPreview();
 
             _textureView.Rotation = 90.0f;
 
-            _tesseractApi = new TesseractApi(ApplicationContext, AssetsDeployment.OncePerVersion);
+            _bitmapPixelArray = new int[_previewSize.Width * _previewSize.Height];
 
-            await _tesseractApi.Init("rus");
         }
 
         public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
@@ -187,18 +186,27 @@ namespace RecognizerTestApp
             if (!_recognizingTextInProgress)
             {
                 _recognizingTextInProgress = true;
-                await Task.Factory.StartNew(RecognizeText);
+                await System.Threading.Tasks.Task.Factory.StartNew(RecognizeText);
+
                 _overlayView.ForceLayout();
             }
         }
 
-        private async Task RecognizeText()
+        private async System.Threading.Tasks.Task InitTesseract()
+        {
+            _tesseractApi = new TesseractApi(ApplicationContext, AssetsDeployment.OncePerVersion);
+
+            await _tesseractApi.Init("rus");
+        }
+
+        private async System.Threading.Tasks.Task RecognizeText()
         {
             //Stopwatch timer = Stopwatch.StartNew();
 
-            Bitmap updatedBitmap = _textureView.GetBitmap(_textureView.Bitmap.Width, _textureView.Bitmap.Height);
+            var updatedBitmap = _textureView.GetBitmap(_textureView.Bitmap.Width, _textureView.Bitmap.Height);
 
-            updatedBitmap.GetPixels(_bitmapPixelArray, 0, _previewSize.Width, 0, 0, _previewSize.Width, _previewSize.Height);
+            updatedBitmap.GetPixels(_bitmapPixelArray, 0, _previewSize.Width, 0, 0, _previewSize.Width,
+                _previewSize.Height);
 
             _tempRecognitionResult.Invalidate();
             _finalRecognitionResult.Invalidate();
@@ -224,17 +232,14 @@ namespace RecognizerTestApp
                     await PerformRecognizing(updatedBitmap);
 
                     if (_tempRecognitionResult.Quality > _finalRecognitionResult.Quality)
-                    {
                         UpdateFinalRecognitionResult();
-                    }
                 }
             }
 
-
             _overlayView.Rect = new Rect(
-                _previewSize.Height - _finalRecognitionResult.BoundingBox.Bottom, 
-                _finalRecognitionResult.BoundingBox.Left, 
-                _previewSize.Height - _finalRecognitionResult.BoundingBox.Top, 
+                _previewSize.Height - _finalRecognitionResult.BoundingBox.Bottom,
+                _finalRecognitionResult.BoundingBox.Left,
+                _previewSize.Height - _finalRecognitionResult.BoundingBox.Top,
                 _bBox.Right);
 
             updatedBitmap.Dispose();
@@ -242,8 +247,33 @@ namespace RecognizerTestApp
             //timer.Stop();
             //TimeSpan timespan = timer.Elapsed;
 
-            _textView.Text = $"Качество: {_finalRecognitionResult.Quality}%";
+            _textView.Text = $"{CommonResources.common_quality}: {_finalRecognitionResult.Quality}%";
             _recognizingTextInProgress = false;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.toolbar_menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_client:
+                    SupportActionBar.Title = CommonResources.on_client;
+                    Toast.MakeText(this, CommonResources.on_client, ToastLength.Short).Show();
+                    _recognizingActor = RecognizingActor.Client;
+                    break;
+                case Resource.Id.menu_server:
+                    SupportActionBar.Title = CommonResources.on_server;
+                    Toast.MakeText(this, CommonResources.on_server, ToastLength.Short).Show();
+                    _recognizingActor = RecognizingActor.Server;
+                    break;
+            }
+
+            return base.OnOptionsItemSelected(item);
         }
 
         private void UpdateFinalRecognitionResult()
@@ -253,7 +283,7 @@ namespace RecognizerTestApp
             _finalRecognitionResult.BoundingBox = new Rect(_tempRecognitionResult.BoundingBox);
         }
 
-        private async Task PerformRecognizing(Bitmap updatedBitmap)
+        private async System.Threading.Tasks.Task PerformRecognizing(Bitmap updatedBitmap)
         {
             GetCroppingBoundingBox(_previewSize.Height, _previewSize.Width);
 
@@ -274,14 +304,21 @@ namespace RecognizerTestApp
                             false);
                 }
 
-                //await TesseractTextRecognizing(croppedBitmap);
-
-                await FirebaseTextRecognizing(croppedBitmap);
+                switch (_recognizingActor)
+                {
+                    case RecognizingActor.Client:
+                        await TesseractTextRecognizing(croppedBitmap);
+                        break;
+                    case RecognizingActor.Server:
+                        await FirebaseTextRecognizing(croppedBitmap);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
 
                 //ExportBitmapAsPNG(cropped);
 
                 croppedBitmap.Dispose();
-               
             }
             else
             {
@@ -289,13 +326,12 @@ namespace RecognizerTestApp
             }
         }
 
-        private async Task FirebaseTextRecognizing(Bitmap croppedBitmap)
+        private async System.Threading.Tasks.Task FirebaseTextRecognizing(Bitmap croppedBitmap)
         {
-            var tcs = new TaskCompletionSource<bool>();
+            var tcs = new System.Threading.Tasks.TaskCompletionSource<bool>();
 
             void OnTextRecognized(object sender, string recognizedText)
             {
-                // do some sht
                 _firebaseProcessImageListener.TextRecognized -= OnTextRecognized;
 
                 recognizedText = recognizedText.Replace("\n", "").Replace(" ", "").ToLower();
@@ -317,7 +353,7 @@ namespace RecognizerTestApp
             await tcs.Task;
         }
 
-        private async Task TesseractTextRecognizing(Bitmap croppedBitmap)
+        private async System.Threading.Tasks.Task TesseractTextRecognizing(Bitmap croppedBitmap)
         {
             if (_tesseractApi != null && _tesseractApi.Initialized)
             {
@@ -335,7 +371,7 @@ namespace RecognizerTestApp
             if (text.Length > 0)
             {
                 double diff = StringDistance.GetDamerauLevenshteinDistance(_referenceString, text);
-                quality = (((double)(_referenceString.Length - diff)) / _referenceString.Length) * 100;
+                quality = (_referenceString.Length - diff) / _referenceString.Length * 100;
             }
 
             _tempRecognitionResult.ResultText = text;
@@ -343,15 +379,18 @@ namespace RecognizerTestApp
             _tempRecognitionResult.BoundingBox = new Rect(_bBox);
         }
 
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            _defaultFirebaseApp = FirebaseApp.InitializeApp(ApplicationContext);
 
             SetContentView(Resource.Layout.activity_main);
             _textView = FindViewById<TextView>(Resource.Id.text_view);
+
+            _toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(_toolbar);
+            SupportActionBar.Title = CommonResources.on_client;
 
             _textureView = FindViewById<TextureView>(Resource.Id.surface_view);
             _overlayView = FindViewById<OverlayView>(Resource.Id.overlay_view);
@@ -374,10 +413,18 @@ namespace RecognizerTestApp
                 ActivityCompat.RequestPermissions(this, new[] {Manifest.Permission.Internet},
                     REQUEST_INTERNET_ID);
 
+            InitFirebase();
+            await InitTesseract();
+        }
+
+        private void InitFirebase()
+        {
+            _defaultFirebaseApp = FirebaseApp.InitializeApp(ApplicationContext);
+
             _firebaseProcessImageListener = new ProcessImageListener();
 
             var options = new FirebaseVisionCloudTextRecognizerOptions.Builder()
-                .SetLanguageHints(new List<string> { "ru" })
+                .SetLanguageHints(new List<string> {"ru"})
                 .SetModelType(FirebaseVisionCloudTextRecognizerOptions.DenseModel)
                 .Build();
 
@@ -387,50 +434,45 @@ namespace RecognizerTestApp
 
         private void GetCroppingBoundingBox(int updateBitmapHeight, int updateBitmapWidth)
         {
-            _bBox.Set(int.MaxValue,int.MaxValue,int.MinValue,int.MinValue);
+            _bBox.Set(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
 
             for (var j = 0; j < updateBitmapHeight; j++)
+            for (var i = 0; i < updateBitmapWidth; i++)
             {
-                for (var i = 0; i < updateBitmapWidth; i++)
-                {
-                    var pixelColor = _bitmapPixelArray[j * updateBitmapWidth + i];
+                var pixelColor = _bitmapPixelArray[j * updateBitmapWidth + i];
 
-                    var red = Color.GetRedComponent(pixelColor);
+                var red = Color.GetRedComponent(pixelColor);
 
-                    // applicableColors.AddRange(_referenceColors.Where(x => Math.Abs(x.R - red) < _currentAccuracy));
+                // applicableColors.AddRange(_referenceColors.Where(x => Math.Abs(x.R - red) < _currentAccuracy));
 
-                    //if (Math.Abs(_referenceColor.R - red) >= _currentAccuracy)
-                    //    //  if (!applicableColors.Any())
-                    //    continue;
+                //if (Math.Abs(_referenceColor.R - red) >= _currentAccuracy)
+                //    //  if (!applicableColors.Any())
+                //    continue;
 
-                    var green = Color.GetGreenComponent(pixelColor);
+                var green = Color.GetGreenComponent(pixelColor);
 
-                    //applicableColors = applicableColors.Except(applicableColors.Where(x => Math.Abs(x.G - green) >= _currentAccuracy)).ToList();
+                //applicableColors = applicableColors.Except(applicableColors.Where(x => Math.Abs(x.G - green) >= _currentAccuracy)).ToList();
 
 
-                    //if (Math.Abs(_referenceColor.G - green) >= _currentAccuracy)
-                    //    //if (!applicableColors.Any())
-                    //    continue;
+                //if (Math.Abs(_referenceColor.G - green) >= _currentAccuracy)
+                //    //if (!applicableColors.Any())
+                //    continue;
 
-                    var blue = Color.GetBlueComponent(pixelColor);
+                var blue = Color.GetBlueComponent(pixelColor);
 
-                    //applicableColors = applicableColors.Except(applicableColors.Where(x => Math.Abs(x.B - blue) >= _currentAccuracy)).ToList();
+                //applicableColors = applicableColors.Except(applicableColors.Where(x => Math.Abs(x.B - blue) >= _currentAccuracy)).ToList();
 
-                    //if (Math.Abs(_referenceColor.B - green) >= _currentAccuracy)
-                    //    //if (!applicableColors.Any())
-                    //    continue;
+                //if (Math.Abs(_referenceColor.B - green) >= _currentAccuracy)
+                //    //if (!applicableColors.Any())
+                //    continue;
 
-                    var dbl_test_red = Math.Pow(((double)_referenceColor.R - red), 2.0);
-                    var dbl_test_green = Math.Pow(((double)_referenceColor.G - green), 2.0);
-                    var dbl_test_blue = Math.Pow(((double)_referenceColor.B - blue), 2.0);
+                var dbl_test_red = Math.Pow((double) _referenceColor.R - red, 2.0);
+                var dbl_test_green = Math.Pow((double) _referenceColor.G - green, 2.0);
+                var dbl_test_blue = Math.Pow((double) _referenceColor.B - blue, 2.0);
 
-                    var distance = Math.Sqrt(dbl_test_blue + dbl_test_green + dbl_test_red);
+                var distance = Math.Sqrt(dbl_test_blue + dbl_test_green + dbl_test_red);
 
-                    if (distance < _currentAccuracy)
-                    {
-                        UpdateBoundingBox(i, j);
-                    }
-                }
+                if (distance < _currentAccuracy) UpdateBoundingBox(i, j);
             }
         }
 
@@ -461,6 +503,12 @@ namespace RecognizerTestApp
             var stream = new FileStream(filePath, FileMode.Create);
             bitmap.Compress(Bitmap.CompressFormat.Png, 100, stream);
             stream.Close();
+        }
+
+        private enum RecognizingActor
+        {
+            Client,
+            Server
         }
     }
 }
